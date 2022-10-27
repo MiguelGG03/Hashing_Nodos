120283d1332dbad008b8c1944f9a90f50baec24b
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-10-27 10:55:08 +0200

    casi todo creado, me voy al gym

---
+++ clases/hash_tables.py
@@ -0,0 +1,47 @@
+
+class Hash:
+
+    def __init__(self):
+        self.tabla=[]
+        self.tamanio=0
+
+    def bernstein(self,cadena):
+        '''Solo Funciona para cadenas de caracteres'''
+        h=0
+        for caracter in cadena:
+            h = h *33 + ord(caracter)
+        return h
+
+    def crear_tabla(self,tamanio):
+        self.tabla=[None]*tamanio
+        self.tamanio=tamanio
+        return self.tabla
+    
+    def cantidad_elementos(self):
+        return self.tamanio-self.tabla.count(None)
+
+    def hasher(self,dato):
+        return self.bernstein(str(dato))% self.tamanio
+    
+    def agregar(self,tabla,dato):
+        posicion=self.hasher(dato)
+
+
+
+
+
+
+
+def main():
+    hashing=Hash()
+    n=int(input('Introduzca el tamaño de la tabla deseado: '))
+    
+    tabla_hash=(hashing.crear_tabla(n))
+    cadena=input('Cadena: ')
+    print(hashing.bernstein(cadena))
+    print(tabla_hash)
+    
+
+
+if __name__=='__main__':
+    main()
